9c5a24eae5d9c3f3c5e39934a3e4b08c7677d777
Loooc <lucav@student.ethz>
2017-04-10 13:54:51 +0200

    13.54.10.04.17

--- app/src/main/java/com/example/lucav/ahopefullyfungame/GameObject.java
+++ app/src/main/java/com/example/lucav/ahopefullyfungame/GameObject.java
@@ -1,8 +1,12 @@
 package com.example.lucav.ahopefullyfungame;
 
+import android.graphics.Canvas;
+
 /**
  * Created by lucav on 10.04.2017.
  */
 
 public interface GameObject {
+    public void draw(Canvas canvas);
+    public void update();
 }
